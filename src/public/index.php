0b435171db91b9a6a76560a1ed7a302ff58bf971
klco <klco@iad.sk>
2026-01-22 09:37:21 +0100

    php fix

--- src/public/index.php
+++ src/public/index.php
@@ -3,29 +3,26 @@ require '../vendor/autoload.php';
 
 use Core\App;
 
-if ((bool) getenv('DEBUG') == true) {
+define('SERVICE_NAME', getenv('SERVICE_NAME'));
+define('SERVICE_URL', getenv('SERVICE_URL'));
+define('SERVICE_LOGO', SERVICE_URL . '/img/logo.webp');
+
+define('PG_HOST', getenv('PG_HOST'));
+define('PG_HOST_PUBLIC', getenv('PG_HOST_PUBLIC'));
+define('PG_PORT', getenv('PG_PORT'));
+define('PG_ADMIN_USER', getenv('PG_USR'));
+define('PG_ADMIN_PASSWORD', getenv('PG_PSW'));
+
+define('SMTP', getenv('STMP'));
+define('SMTP_username', getenv('SMTP_USR'));
+define('SMTP_password', getenv('STMP_PSW'));
+define('SMTP_email_from', getenv('STMP_MAILFROM'));
+
+if (filter_var(getenv('DEBUG'), FILTER_VALIDATE_BOOLEAN)) {
     ini_set('display_errors', '1');
-    ini_set('display_startup_errors', 1);
+    ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
 }
-
-const SERVICE_NAME = getenv('SERVICE_NAME');
-const SERVICE_URL = getenv('SERVICE_URL');
-const SERVICE_LOGO = SERVICE_URL . '/img/logo.webp';
-
-const PG_HOST = getenv('PG_HOST');
-const PG_HOST_PUBLIC = getenv('PG_HOST_PUBLIC');
-const PG_PORT = getenv('PG_PORT');
-const PG_ADMIN_USER = getenv('PG_USR');
-const PG_ADMIN_PASSWORD = getenv('PG_PSW');
-
-const SMTP = getenv('STMP');
-const SMTP_username = getenv('SMTP_USR');
-const SMTP_password = getenv('STMP_PSW');
-const SMTP_email_from = getenv('STMP_MAILFROM');
-
-const BASE_PATH = __DIR__ . "/../";
-const VIEWS_PATH = BASE_PATH . "views/";
 
 session_start();
 
